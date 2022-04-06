260e433a2768053266c37af7064fc86b78232910
Cauã <cauabarbo@gmail.com>
2022-04-06 11:55:45 -0300

    Added labels for the tabs and the buttons

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time, threading
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
+
 
 class MainTimer:
 
@@ -21,9 +22,33 @@ class MainTimer:
         self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
         self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
 
+        self.pomodoro_timer_label = ttk.Label(self.tab1, text='25:00', font=('Ubuntu', 48))
+        self.pomodoro_timer_label.pack(pady=20)
+
+        self.pomodoro_timer_label = ttk.Label(self.tab2, text='05:00', font=('Ubuntu', 48))
+        self.pomodoro_timer_label.pack(pady=20)
+
+        self.pomodoro_timer_label = ttk.Label(self.tab3, text='10:00', font=('Ubuntu', 48))
+        self.pomodoro_timer_label.pack(pady=20)
+        
+
         self.tabs.add(self.tab1, text='Pomodoro')
         self.tabs.add(self.tab2, text='SB')
         self.tabs.add(self.tab3, text='LB')
+
+        self.grid_layout = ttk.Frame(self.root)
+        self.grid_layout.pack(pady=10)
+
+        self.start_button = ttk.Button(self.grid_layout, text='Start', command=self.start_timer_thread)
+        self.start_button.grid(row=0, column=0)
+
+        self.skip_button = ttk.Button(self.grid_layout, text='Skip', command=self.skip_timer)
+        self.skip_button.grid(row=0, column=1)
+
+        self.reset_button = ttk.Button(self.grid_layout, text='Reset', command=self.reset_timer)
+        self.reset_button.grid(row=0, column=2)
+
+        
         
         self.root.mainloop()
 
